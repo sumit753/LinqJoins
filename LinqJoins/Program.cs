ba3eb683fdbe60fcf27755d0a9913863f3627960
sumit753 <sumit753@gmail.com>
2017-03-15 16:31:14 +0530

    Joins(Inner & Outer Joins)

--- LinqJoins/Program.cs
+++ LinqJoins/Program.cs
@@ -10,6 +10,60 @@ namespace LinqJoins
     {
         static void Main(string[] args)
         {
+
+            //group join example
+            var employeeByDepartment = Department.GetAllDepartments().GroupJoin(Employee.GetAllEmployees()
+                                            , d => d.ID, e => e.DepartmentID, (department, employee) => new
+                                            {
+                                                Department = department,
+                                                Employee = employee.OrderBy(e => e.Name)
+
+                                           });
+            // as sql
+
+            var employeeByDept = from d in Department.GetAllDepartments()
+                                 join e in Employee.GetAllEmployees()
+                                 on d.ID equals e.DepartmentID into egroups
+                                 select new
+                                 {
+                                     Department = d,
+                                     Employee = egroups
+                                 };
+
+
+            foreach (var obj in employeeByDept)
+            {
+
+                Console.WriteLine(obj.Department.Name);
+
+                foreach (var employee in obj.Employee)
+                {
+                    Console.WriteLine(" "+employee.Name);
+                }
+            }
+
+            //Inner joins
+
+            var innerjoinResult = Department.GetAllDepartments().Join(Employee.GetAllEmployees(),d=>d.ID ,e=>e.DepartmentID,
+                                  (department, employee)=> new {
+                                                                  Deparment = department,
+                                                                  Employee = employee
+                                                                       
+                                                                }).GroupBy(d=>d.Deparment.Name).Select(obj => new { department = obj.Key,Employee = obj});
+
+            
+            foreach (var obj in innerjoinResult)
+            {
+
+                Console.WriteLine(obj.department);
+                foreach (var e in obj.Employee)
+                {
+                    Console.WriteLine(" "+e.Employee.Name);
+                }
+               
+            }
+
+
         }
     }
 }
